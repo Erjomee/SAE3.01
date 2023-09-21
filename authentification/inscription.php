b077b2aa974fc3b4e0f9fcd72f3733cfacf7c4e6
IdekiFR <jerome.tran2004@outlook.fr>
2023-09-21 14:34:39 +0200

    commit

--- authentification/inscription.php
+++ authentification/inscription.php
@@ -1,19 +1,32 @@
 <?php  
-
+    if req
     $name = $_POST["nom"] ; 
     $surname = $_POST["prenom"] ; 
     $email = $_POST["email"] ; 
     $password = $_POST["password"] ;    
     include("connectBD.php");
     try {
-        $stmt = $pdo->prepare('INSERT');
-        $stmt->execute();
 
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo 'Nom: ' . $row['nom'] . ', Prenom: ' . $row['prenom'] . ', Email: ' . $row['email'] . $row['numero'].'<br>';
-            }
+        if ($_SERVER['REQUEST_ METHOD'] == 'POST'){
+            $pdo ->beginTransaction();
+            
+            $stmt = $pdo->prepare('INSERT INTO utilisateur(nom, prenom,email,password) VALUES (:nom , :prenom , :email , :password)');
+            $stmt->execute([
+                ':nom' => $name,
+                ':prenom' => $surname,
+                ':email' => $email,
+                ':password' => $password
+            ]);
 
-        
+            $stmt->execute();
+            $stmt->commit();
+
+            echo "transac réussis"
+
+            // while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            //     echo 'Nom: ' . $row['nom'] . ', Prenom: ' . $row['prenom'] . ', Email: ' . $row['email'] . $row['numero'].'<br>';
+            //     }
+        }    
     } catch (Exeption $e) {
         // En cas d'erreur, annulez la transaction
         $pdo->rollBack();
