d2f6c4f02e7e80268eb6015bcbbfd4b9946c1f47
IdekiFR <jerome.tran2004@outlook.fr>
2023-09-21 02:19:11 +0200

    récupération données formulaire

---
+++ inscription.php
@@ -0,0 +1,10 @@
+<?php  
+
+    $name = $_POST["nom"] ; 
+    $surname = $_POST["prenom"] ; 
+    $email = $_POST["email"] ; 
+    $password = $_POST["password"] ; 
+
+
+    echo "$name<br>$surname<br>$email<br>$password"
+?>
