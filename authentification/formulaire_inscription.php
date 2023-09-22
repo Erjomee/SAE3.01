4d087402ce3ab82695f3a067d1407f0a81e1d23c
IdekiFR <jerome.tran2004@outlook.fr>
2023-09-22 02:49:03 +0200

    information additionnel

--- authentification/formulaire_inscription.php
+++ authentification/formulaire_inscription.php
@@ -16,56 +16,87 @@
     <fieldset>
         <legend>Inscription</legend>
         <p>
-            <label for="nom">Nom</label>
+            <label for="nom">Nom*:</label>
             <input type="text" id="nom_id" name="nom" placeholder="Dupont" required>
         </p>
         <p>
-            <label for="prenom">Pénom</label>
+            <label for="prenom">Pénom*:</label>
             <input type="text" id="prenom_id" name="prenom" placeholder="Jean" required>
         </p>
         <p>
-            <label for="email">Email</label>
+            <label for="email">Email*:</label>
             <input type="email" id="email_id" name="email" placeholder="jean.dupont@gmail.com" required>
         </p>
         <p>
-            <label for="prenom">Mot de passe</label>
+            <label for="prenom">Mot de passe*:</label>
             <input type="password" id="password_id" name="password" placeholder="mdp123" required>
+        </p>
+        <p>
+            <label for="prenom">Numéro:</label>
+            <input type="tel" id="numero_id" name="numero" placeholder="0629701938">
+        </p>
+        <p>
+            <label for="prenom">Sexe:</label>
+            <input type="radio" name="sexe" value="M">Homme
+            <input type="radio" name="sexe" value="F">Femme
         </p>
 
         <p style="display: none;" id="error">Utilisateur déjà existant</p>
     
-        <button name="ok" > Soumettre </button>
+        <input type="submit" name="submit_btn" value="Soumettre">
     </fieldset>
 </form>
 
 <?php 
     // Récupération des information du formulaire
-    if (isset($_POST['ok'])) {
-        $name = $_POST["nom"] ; 
-        $surname = $_POST["prenom"] ; 
-        $email = $_POST["email"] ; 
-        $password = $_POST["password"] ;  
-
+    if (isset($_POST['submit_btn'])) { 
         // Connection à la base de donnée
         include("connectBD.php");
         try {
-            $sql = "Select * from utilisateur where email = '$email'";
+
+            $utilisateur = array(
+                "nom" => $_POST["nom"] ,
+                "prenom" => $_POST["prenom"],
+                "email" => $_POST["email"],
+                "password" => $_POST["password"]
+            );
+
+            $sql = "Select * from utilisateur where email = '$utilisateur[email]'";
             $reponse = $pdo->query($sql);
             // Si l'utilisateur n'est pas encore inscrit
             if ($reponse->rowCount() == 0) {
+
+                $columns = "nom,prenom,email,password";
+                $values = ":nom,:prenom,:email,:password";
+
+                $dico_value = [
+                    ':nom' => $utilisateur["nom"],
+                    ':prenom' => $utilisateur["prenom"],
+                    ':email' => $utilisateur["email"],
+                    ':password' => $utilisateur["password"]
+                ];
+
+                if (isset($_POST['numero'])) {
+                    $utilisateur["numero"] = $_POST["numero"];
+                    $columns .= ",numero";
+                    $values .= ",:numero";
+                    $dico_value["numero"] = $utilisateur["numero"];
+                }
+                if (isset($_POST['sexe'])) {
+                    $utilisateur["sexe"] = $_POST["sexe"];
+                    $columns .= ",sexe";
+                    $values .= ",:sexe";
+                    $dico_value["sexe"] = $utilisateur["sexe"];
+
+                }
                 $pdo ->beginTransaction();
                 
-                $stmt = $pdo->prepare('INSERT INTO utilisateur(nom, prenom,email,password) VALUES (:nom , :prenom , :email , :password)');
-                $stmt->execute([
-                    ':nom' => $name,
-                    ':prenom' => $surname,
-                    ':email' => $email,
-                    ':password' => $password
-                ]);
+                $stmt = $pdo->prepare("INSERT INTO utilisateur($columns) VALUES ($values)");
+                $stmt->execute($dico_value);
 
                 $pdo->commit();
                 
-                $_SESSION['user'] = $name;
+                $_SESSION['user'] = $utilisateur["nom"];
                 header('Location: http://127.0.0.1/SAE3.01/');
                 
 
@@ -78,7 +109,7 @@
                 </script> ";
             }
             
-        } catch (Exeption $e) {
+        } catch (Exception $e) {
             // En cas d'erreur, annulez la transaction
             $pdo->rollBack();
             echo 'Erreur : ' . $e->getMessage();
