982462667ed7f0ee899712e7cb8116109f7ada8b
IdekiFR <jerome.tran2004@outlook.fr>
2023-09-25 00:32:52 +0200

    commit

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// Pour faire basculer la nav barre au click du menu déroulant
-document.querySelector(".menu").addEventListener("click",function () {
-    document.getElementById('menu-icon').classList.toggle("bx-x");
-    document.querySelector(".nav-list").classList.toggle("open")
-    document.querySelector(".user-list").classList.remove("open")
-    document.querySelector(".utilisateur").classList.remove("white-bg")
-    document.querySelector(".menu").classList.toggle("gray-bg");
-})
+// On vérifie si la zone utilisateur existe (si l'utilisateur n'est pas invité)
+if (document.querySelector(".utilisateur") !== null){ 
+    // Gestion du clic sur le lenu icon
+    document.querySelector(".menu").addEventListener("click",function () {
+        document.getElementById('menu-icon').classList.toggle("bx-x");
+        document.querySelector(".nav-list").classList.toggle("open")
+        document.querySelector(".user-list").classList.remove("open")
+        document.querySelector(".utilisateur").classList.remove("white-bg")
+        document.querySelector(".menu").classList.toggle("gray-bg");
+    })
 
+    // Gestion du clic sur le user icon
+    document.querySelector(".utilisateur").addEventListener("click",function () {
+        document.querySelector(".utilisateur").classList.toggle("white-bg");
+        document.querySelector(".user-list").classList.toggle("open");
+        document.querySelector(".nav-list").classList.remove("open")
+        document.getElementById('menu-icon').classList.remove("bx-x");
+        document.querySelector(".menu").classList.remove("gray-bg")
+    })
 
-// Pour faire basculer la nav barre au click du menu déroulant
-document.querySelector(".utilisateur").addEventListener("click",function () {
-    document.querySelector(".utilisateur").classList.toggle("white-bg");
-    document.querySelector(".user-list").classList.toggle("open");
-    document.querySelector(".nav-list").classList.remove("open")
-    document.getElementById('menu-icon').classList.remove("bx-x");
-    document.querySelector(".menu").classList.remove("gray-bg")
-})
-
-
-// Zone utilisateure
-document.querySelector(".utilisateur").addEventListener("mouseenter",function () {
-    document.getElementById('user-icon').classList.add("bx-tada");
-})
-document.querySelector(".utilisateur").addEventListener("mouseleave",function () {
-    document.getElementById('user-icon').classList.remove("bx-tada");
-})
-
+    // Animation icon user
+    document.querySelector(".utilisateur").addEventListener("mouseenter",function () {
+        document.getElementById('user-icon').classList.add("bx-tada");
+    })
+    document.querySelector(".utilisateur").addEventListener("mouseleave",function () {
+        document.getElementById('user-icon').classList.remove("bx-tada");
+    })
+}
