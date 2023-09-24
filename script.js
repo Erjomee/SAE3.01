eb5975a92db55ceaa084c8291f3b9b4ad728ad51
IdekiFR <jerome.tran2004@outlook.fr>
2023-09-24 14:49:15 +0200

    commit

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 document.getElementById('menu-icon').addEventListener("click",function () {
     document.getElementById('menu-icon').classList.toggle("bx-x");
     document.querySelector(".nav-list").classList.toggle("open")
-});
+})
 
-
-document.getElementById('utilisateur').addEventListener("click",function () {
+// feozbfoezf
+document.querySelector(".utilisateur").addEventListener("click",function () {
     document.getElementById('user-icon').classList.toggle("bx bxs-user-circle bx-tada");
     console.log("lefole")
 })
