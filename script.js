9c2c682e6bca39e8dac6bca7cc135de3bb204ef4
IdekiFR <jerome.tran2004@outlook.fr>
2023-09-24 14:57:15 +0200

    commit

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// Pour faire bascuuler la nav barre au click du menu déroulant
+// Pour faire basculer la nav barre au click du menu déroulant
 document.getElementById('menu-icon').addEventListener("click",function () {
     document.getElementById('menu-icon').classList.toggle("bx-x");
-    document.querySelector(".nav-list").classList.toggle("open")
+    document.querySelector(".nav-list").classList.toggle("open");
+    console.log("fefe");
 })
 
-// feozbfoezf
 document.querySelector(".utilisateur").addEventListener("click",function () {
     document.getElementById('user-icon').classList.toggle("bx bxs-user-circle bx-tada");
-    console.log("lefole")
 })
