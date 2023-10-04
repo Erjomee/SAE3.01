6072b835bb38f8c571de1f8756654c6636012959
kleper240 <mabulu.marius33@gmail.com>
2023-10-04 10:42:00 +0200

    ajoute des fichier

---
+++ controllers/Router.php
@@ -0,0 +1,43 @@
+<?php
+
+    class Router{
+        private $_ctrl ;
+        private $_view ;
+
+        public function routeReq(){
+            try{
+                spl_autoload_register(function($class){
+                    require_once ("models/". $class . ".php");
+                    $url = " ";
+                    if (isset($_GET[$url])){
+                        $url = explode('/', filter_var($_GET[$url], FILTER_SANITIZE_URL));
+                        $controller = ucfirst(strtolower($url[0]));
+
+                        $controllerClass = "controller".$controller;
+                        $conllerFile = "controllers/" . $conllerClass .".php";
+
+                        if(file_exists($controllerFile)){
+                            require_once($conllerFile);
+                            $this->_ctrl = new $controllerClass($url);
+                        }
+                        else{
+                            throw new Exception("page introuvable");
+                        }
+
+                    }
+                    else{
+                        require_once("controllers/controllersAccueil.php");
+                        $controllerClass = "controllersAccueil";
+                        $this->_ctrl = new $conllerClass($url);
+                    }
+                });
+            }
+            catch(Exception $e){
+                    $errorMessage = $e->getMessage();
+                    require_once("views/viewError.php");
+
+            }
+        }
+}
+
+?>
---
+++ user/profil.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/profil.css">
+    <title>profil</title>
+</head>
+<body>
+
+
+
+
+<form action="" method="POST">
+
+    <div class = "menu">
+
+    </div>
+
+    <div class= "deux">
+        <div class="ber">
+
+        </div>
+    </div>
+
+    <div class="gros">
+        <p>Mon compte</p>
+    </div>
+
+    
+    <div>
+        <legend>Connexion</legend>
+        <div>
+            <label for="nom">nom:</label>
+            <input type="text" name="nom" id="nom">
+
+            <label for="prenom">prenom:</label>
+            <input type="text" name="prenom" id="prenom">
+
+            <label for="mail">email:</label>
+            <input type="email" name="mail" id="mail">
+
+            <label for="passwrd">password:</label>
+            <input type="password" name="passwrd" id="passwrd">
+
+            <label for="sexe">sexe:</label>
+            <input type="text" name="sexe" id="sexe">
+
+            <input type="submit" value="valider">
+        </div>
+    </div>
+
+    <div>
+        <div>
+            <label for="desc">A propos de moi: </label>
+            <input type="text" name="desc" id="desc">
+        </div>
+    </div>
+
+    <footer>
+
+
+    </footer>
+
+</form>
+</body>
+</html>
