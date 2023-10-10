79610945d15d3520278ad2135f277603c9f28eba
IdekiFR <jerome.tran2004@outlook.fr>
2023-10-10 10:07:09 +0200

    test

--- decouvrir.php
+++ decouvrir.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-    <title>Découvrir</title>
+    <title>Découvrirs</title>
 </head>
 <?php include("assets/php/nav.php"); ?>
 <body>
