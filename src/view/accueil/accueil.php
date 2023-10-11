39c91e9228b8a409469d57526f0307a2b69df54a
electra04 <electraaa.aaaa1@gmail.com>
2023-10-11 14:59:47 +0200

    barre de recherche (accueil.php et style.css)

--- src/view/accueil/accueil.php
+++ src/view/accueil/accueil.php
@@ -1,6 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet"href="../../assets/css/style.css">
+</head>
+<body>
+<form>
+        <nav class="nav_recherche">
+            <input type="search" id="marecherche" name="recherche" placeholder="Rechercher sur le site" required>
+            <button type="submit">Rechercher</button>
+            <span class="validity"></span>
+        </nav>
+    </form>
+
+
+
+
+
 <?php
 
 
     echo "<h1> Accueil </h1>";
 
 ?>
+
+</body>
