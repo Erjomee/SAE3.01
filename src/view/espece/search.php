a03de2b22587687296a37af1e8cf09570217e823
IdekiFR <jerome.tran2004@outlook.fr>
2023-10-13 19:04:04 +0200

    html et css (page espece)

--- src/view/espece/search.php
+++ src/view/espece/search.php
@@ -1,15 +1,23 @@
 
-<div class="espece">
+
+<div class="container">
 
     <div id="filtre">
-        
+        <h4>
+            Affinez votre choix
+        </h4>
+        <h5>Recherche par:</h5>
+        <ul>
+            <li><input type="radio" name="filtre" value="id_espece">ID espece</li>
+            <li><input type="radio" name="filtre" value="vernacular_name" checked>Nom vernaculaire</li>
+            <li><input type="radio" name="filtre" value="scientific_name">Nom scientifique</li>
+        </ul>
     </div>
 
-    <div class="main">
+    <div class="search">
         <h1>
-            ESPECE
+            ESPECES
         </h1>
-
 
         <form>
             <nav class="nav_recherche">
@@ -17,10 +25,50 @@
                 <button type="submit">Rechercher</button>
                 <span class="validity"></span>
             </nav>
-        </form>
-    </div>
 
+        </form>
+
+        <hr>
+        <h3>
+            Dernières espèces recherchés :
+        </h3>
+        <div class="historique">
+            <!--   à automatiser avec php (mettre les images)         -->
+            <div class="item">
+                Item 1
+            </div>
+            <div class="item">
+                Item 2
+            </div>
+            <div class="item">
+                Item 3
+            </div>
+            <div class="item">
+                Item 4
+            </div>
+            <div class="item">
+                Item 5
+            </div>
+
+            <div class="item">
+                Item 6
+            </div>
+            <div class="item">
+                Item 7
+            </div>
+            <div class="item">
+                Item 8
+            </div>
+            <div class="item">
+                Item 9
+            </div>
+            <div class="item">
+                Item 10
+            </div>
+        </div>
+    </div>
 </div>
+
 
 
 
