876b835b396a1e91c59998b649e17264f2fb2e82
IdekiFR <jerome.tran2004@outlook.fr>
2023-10-14 01:34:23 +0200

    html et css (page espece)

--- src/view/espece/search.php
+++ src/view/espece/search.php
@@ -2,35 +2,49 @@
 
 <div class="container">
 
-    <div id="filtre">
-        <h4>
-            Affinez votre choix
-        </h4>
-        <h5>Recherche par:</h5>
-        <ul>
-            <li><input type="radio" name="filtre" value="id_espece">ID espece</li>
-            <li><input type="radio" name="filtre" value="vernacular_name" checked>Nom vernaculaire</li>
-            <li><input type="radio" name="filtre" value="scientific_name">Nom scientifique</li>
-        </ul>
-    </div>
+<!--    <div id="filtre">-->
+<!--        <h4>-->
+<!--            Affinez votre choix-->
+<!--        </h4>-->
+<!--        <h5>Recherche par:</h5>-->
+<!--        <form method="get">-->
+<!--            <ul>-->
+<!--                <li><input type="radio" name="filtre_f" value="id_espece">ID espece</li>-->
+<!--                <li><input type="radio" name="filtre_f" value="vernacular_name" checked>Nom vernaculaire</li>-->
+<!--                <li><input type="radio" name="filtre_f" value="scientific_name">Nom scientifique</li>-->
+<!--            </ul>-->
+<!--        </form>-->
+<!--    </div>-->
 
     <div class="search">
         <h1>
             ESPECES
         </h1>
 
-        <form>
+        <form id="rechercheForm"  method="get" >
             <nav class="nav_recherche">
                 <input type="search" id="marecherche" name="recherche" placeholder="Rechercher sur le site">
                 <button type="submit">Rechercher</button>
                 <span class="validity"></span>
             </nav>
-
+            <div id="filtre">
+                <h4>
+                    Affinez votre choix
+                </h4>
+                <h5>Recherche par:</h5>
+                <form method="get">
+                    <ul>
+                        <li><input type="radio" name="filtre_f" value="id_espece">ID espece</li>
+                        <li><input type="radio" name="filtre_f" value="vernacular_name" checked>Nom vernaculaire</li>
+                        <li><input type="radio" name="filtre_f" value="scientific_name">Nom scientifique</li>
+                    </ul>
+                </form>
+            </div>
         </form>
 
         <hr>
         <h3>
-            Dernières espèces recherchés :
+            Espèces recherchés :
         </h3>
         <div class="historique">
             <!--   à automatiser avec php (mettre les images)         -->
