4283fc8c5a1aa13ba1be1961d91418ab8b45acd7
kleper240 <mabulu.marius33@gmail.com>
2023-10-16 14:44:06 +0200

    marius

--- src/view/naturotheque/naturotheque.php
+++ src/view/naturotheque/naturotheque.php
@@ -1,7 +1,21 @@
+<div class="contener1">
+    <h1> Naturothèque </h1>
+
+    <div class="dev" id="contener2">
+        <p id="texte">Mabulu Marius</p>
+
+        <div class="tete" id="contener3">
+            <div class="phtotos" id="contener4">
+
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?php
 
 
-    echo "<h1> Naturothèque </h1>";
+    // echo "<h1> Naturothèque </h1>";
 
-?>
+?> 
