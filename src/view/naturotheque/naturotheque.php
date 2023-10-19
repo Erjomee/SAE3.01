a2402656f2bfafe3e6f6696d94753677246bae90
kleper240 <mabulu.marius33@gmail.com>
2023-10-19 11:39:46 +0200

    partie naturautheque

--- src/view/naturotheque/naturotheque.php
+++ src/view/naturotheque/naturotheque.php
@@ -1,42 +1,19 @@
 <div class="contener1">
-    <h1> Naturothèque </h1>
+        <h1> Naturothèque </h1>
+        <div class="flex-contenaire">
+        <div class="flex-box">box 1</div>
+        <div class="flex-box">box 2</div>
+        <div class="flex-box">box 3</div>
+        <div class="flex-box">box 4</div>
+        <div class="flex-box">box 5</div>
+        <div class="flex-box">box 1</div>
+        <div class="flex-box">box 2</div>
+        <div class="flex-box">box 3</div>
+        <div class="flex-box">box 4</div>
+        <div class="flex-box">box 5</div>
+    </div>
 
-    <!-- <div class="dev" id="contener2">
-        <p id="texte">Mabulu Marius</p>
-
-        <div class="tete" id="contener3">
-            <div class="phtotos" id="contener4">
-
-            </div>
-        </div>
-    </div> -->
-    <li>
-        <ul>
-            <div class="dev" id="contener2">
-                <p id="texte">Mabulu Marius</p>
-
-                <div class="tete" id="contener3">
-                    <div class="phtotos" id="contener4">
-
-                    </div>
-                </div>
-            </div>
-        </ul>
-
-        <ul>
-            <div class="dev" id="contener2">
-                <p id="texte">Mabulu Marius</p>
-
-                <div class="tete" id="contener3">
-                    <div class="phtotos" id="contener4">
-
-                    </div>
-                </div>
-            </div>
-        </ul>
-    </li>
 </div>
-
 
 
 <?php
