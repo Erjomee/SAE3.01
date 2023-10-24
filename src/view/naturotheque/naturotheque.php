f246908a5b6f2b57d74f523d108490ddf67d2f24
kleper240 <mabulu.marius33@gmail.com>
2023-10-24 10:38:52 +0200

    marius

--- src/view/naturotheque/naturotheque.php
+++ src/view/naturotheque/naturotheque.php
@@ -1,19 +1,47 @@
+
 <div class="contener1">
-        <h1> Naturothèque </h1>
-        <div class="flex-contenaire">
-        <div class="flex-box">box 1</div>
-        <div class="flex-box">box 2</div>
-        <div class="flex-box">box 3</div>
-        <div class="flex-box">box 4</div>
-        <div class="flex-box">box 5</div>
-        <div class="flex-box">box 1</div>
-        <div class="flex-box">box 2</div>
-        <div class="flex-box">box 3</div>
-        <div class="flex-box">box 4</div>
-        <div class="flex-box">box 5</div>
+    
+    <h1> Naturothèque </h1>
+    <div class="flex-contenaire">
+        <div class="flex-box">
+            <div class="teste">
+                <h2 class= "identite" >Marius Mabulu</h2>
+            </div>
+            <div class="tete" id="contener3">
+                    <div class="phtotos" id="contener4"></div>
+        </div>
+
+    </div>
+
+    <div class="flex-box">
+        <div class="cart-top">
+            <img src="" alt="">
+        </div>
+        <div class="carteInfo">
+            <h2>Marius Mabulu</h2>
+            <h3>Age : 20 sexe : homme </h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, enim. Eius accusamus 
+                quisquam dignissimos quibusdam fugiat numquam natus tempore! Nostrum dolorum dolores
+                 sequi sed deleniti officiis provident unde nam soluta.
+            </p>
+            <div class="description">
+                <h3> Taille de la naturothèque 9 espèces enregistrées</h3>
+            </div>
+        </div>
+
+    </div>
+    <div class="flex-box">box 3</div>
+    <div class="flex-box">box 4</div>
+    <div class="flex-box">box 5</div>
+    <div class="flex-box">box 1</div>
+    <div class="flex-box">box 2</div>
+    <div class="flex-box">box 3</div>
+    <div class="flex-box">box 4</div>
+    <div class="flex-box">box 5</div>
     </div>
 
 </div>
+
 
 
 <?php
