ba1c177e157851630698ae40d0785cd9b9d98006
kleper240 <mabulu.marius33@gmail.com>
2023-12-08 14:51:29 +0100

    dernier 8/12

--- src/view/naturotheque/naturotheque.php
+++ src/view/naturotheque/naturotheque.php
@@ -15,6 +15,7 @@
                         <h2>{$values['prenom']} {$values['nom']}</h2>
                         <h3>Age : $age  &nbsp;&nbsp;  sexe : {$values['sexe']} </h3>
                     </div>
+                        <button  class='btn'>Découvrir</button>
                     <div>
                         <p>Débloquez ce fichier et bénéficiez d’un accès illimité à plus de 76 235 images Premium. 
                         </p>
@@ -23,9 +24,7 @@
                         <h3> Taille de la naturothèque </h3>
                         <p>9 espèces enregistrées</p>
                     </div>
-                    <div class='info'>
-                        <button  class='btn'>Découvrir</button>
-                    </div>
+
                 </div>";
                 
             }
