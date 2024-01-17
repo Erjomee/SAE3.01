811c7a1351167fa256ba94c126a68225c30d590c
kleper240 <119818975+kleper240@users.noreply.github.com>
2024-01-17 15:10:29 +0100

    Update edit_profil.php

--- src/view/utilisateur/edit_profil.php
+++ src/view/utilisateur/edit_profil.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styles.css">
-  <title>Profile Settings</title>
-</head>
-<body>
-
 <div class="page-container">
   <div class="tab-container">
     <a href="/fr-fr/edit-profile/" class="active">Profil</a>
@@ -76,6 +66,3 @@
     </div>
   </form>
 </div>
-
-</body>
-</html>
