48157c9e54c8e789bb0c3e6d5cb0039caa4a60a5
kleper240 <119818975+kleper240@users.noreply.github.com>
2024-01-19 16:01:38 +0100

    Update ma_naturotheque.php

---
+++ src/view/naturotheque/ma_naturotheque.php
@@ -0,0 +1,15 @@
+<body>
+    <h1>Ma naturautheque</h1>
+
+    <div class="tab-container">
+        <div class="tab" onclick="selectTab('profil')">Profil</div>
+        <div class="tab" onclick="selectTab('notifications')">Notifications</div>
+    </div>
+    
+    <div class="grille">
+        <div class="grid-items"></div>
+
+    </div>
+
+    <script src="script.js"></script>
+</body>
